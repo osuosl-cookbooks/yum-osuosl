2c6fc2469f128e1a08d79de075897661b2a83335
Manatee <github@osuosl.org>
2017-01-06 00:02:34 +0000

    Automatic patch-level version bump to v0.1.2 by Jenkins

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,10 @@ yum-osuosl CHANGELOG
 This file is used to list changes made in each version of the
 yum-osuosl cookbook.
 
+0.1.2 (2017-01-06)
+------------------
+- Fixed rubocop and foodcritic for yum-osuosl
+
 0.1.0
 -----
 - Initial release of yum-osuosl
--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ issues_url       'https://github.com/osuosl-cookbooks/yum-osuosl/issues'
 source_url       'https://github.com/osuosl-cookbooks/yum-osuosl'
 description      'Installs/Configures yum-osuosl'
 long_description 'Installs/Configures yum-osuosl'
-version          '0.1.1'
+version          '0.1.2'
 
 supports 'centos', '~> 6.0'
 supports 'centos', '~> 7.0'
