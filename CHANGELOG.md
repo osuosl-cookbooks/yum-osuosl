13ed84ac6f93cab911c20048f62433da5de26976
Manatee <github@osuosl.org>
2018-09-13 15:40:43 +0000

    Automatic major-level version bump to v1.0.0 by Jenkins

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,10 @@ yum-osuosl CHANGELOG
 This file is used to list changes made in each version of the
 yum-osuosl cookbook.
 
+1.0.0 (2018-09-13)
+------------------
+- Chef 13 compatibility fixes
+
 0.1.2 (2017-01-06)
 ------------------
 - Fixed rubocop and foodcritic for yum-osuosl
--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ issues_url       'https://github.com/osuosl-cookbooks/yum-osuosl/issues'
 source_url       'https://github.com/osuosl-cookbooks/yum-osuosl'
 description      'Installs/Configures yum-osuosl'
 long_description 'Installs/Configures yum-osuosl'
-version          '0.1.2'
+version          '1.0.0'
 
 supports 'centos', '~> 6.0'
 supports 'centos', '~> 7.0'
