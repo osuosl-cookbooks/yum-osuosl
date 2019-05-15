e7717540da6af4c225ebb9d7c110151954bec0b6
Catherine Stevns <stevenscatherine2@gmail.com>
2019-05-15 09:26:49 -0700

    Replaced default_spec file

--- test/integration/default/inspec/default_spec.rb
+++ test/integration/default/inspec/default_spec.rb
@@ -1,1 +1,4 @@
-
+describe yum.repo('osuosl') do
+  it { should exist }
+  it { should be_enabled }
+end
--- test/integration/default/inspec/yum_osuosl_spec.rb
+++
@@ -1,4 +0,0 @@
-describe yum.repo('osuosl') do
-  it { should exist }
-  it { should be_enabled }
-end
