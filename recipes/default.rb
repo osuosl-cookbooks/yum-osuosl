c3e77941e9cb31b0b414dc960af43eac0596b4b6
Tim Smith <tsmith84@gmail.com>
2019-08-07 11:27:49 -0700

    Fix comment format
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,8 +1,8 @@
 #
-# Cookbook Name:: yum-osuosl
+# Cookbook:: yum-osuosl
 # Recipe:: default
 #
-# Copyright (C) 2014 Oregon State University
+# Copyright:: (C) 2014 Oregon State University
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
