a23c2b36aaf0684e1dbb1cef91354ce4a304dec0
Lance Albertson <lance@osuosl.org>
2019-08-09 17:06:55 -0700

    Manual patch bump

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ issues_url       'https://github.com/osuosl-cookbooks/yum-osuosl/issues'
 source_url       'https://github.com/osuosl-cookbooks/yum-osuosl'
 description      'Installs/Configures yum-osuosl'
 long_description 'Installs/Configures yum-osuosl'
-version          '1.0.0'
+version          '1.0.1'
 
 supports 'centos', '~> 6.0'
 supports 'centos', '~> 7.0'
